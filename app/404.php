f399b709af0de32f3ac3bf1468a0a0231c7e592c
Johnathan Lyman <jelyman@gmail.com>
2018-02-10 13:51:14 -0800

    404 page

---
+++ app/404.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<!-- body -->
+    <div class="container">
+        <div class="column">
+            <div class="column-wrapper" id="infinite">
+            
+            <article class="h-entry page h-entry" itemscope itemtype="http://schema.org/BlogPosting">
+            <h1 class="p-name" itemprop="headline">Page Not Found</h1>
+
+            <div class="e-content">
+            <p>
+            What you're looking for isn't here. That's not your problem, though, unless you accessed this page on purpose...
+            </p>
+            </div>
+            </article>
+            <br /><br />
+
+
+            </div>
+        </div>
+    </div>
+<!-- / body -->
+
+<?php get_footer(); ?>
