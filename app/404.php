ff1561f8f69f6f587e9b446bc64408f57d393cd4
Johnathan Lyman <jelyman@gmail.com>
2018-09-29 11:51:56 -0700

    add 404 page

---
+++ app/404.php
@@ -0,0 +1,12 @@
+<!-- template: page -->
+<?php get_header(); ?>
+<!-- body -->
+<div style="text-align: center;">
+<h1>404 - Page Not Found</h1>
+<p>What you're looking for isn't here. That's probably not intended, either. :/</p>
+<p>Your best bet is to head <a href="/">Home</a> and give it another go. </p>
+<p>Before you do that, <a href="/contact">let me know</a> this happened and I'll dig into it.</p>
+</div>
+<!-- / body -->
+<?php get_footer(); ?>
+<!-- / template: page -->
