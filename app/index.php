09103ae8676781a5a29003bee3b2ac645faa1cf6
Johnathan Lyman <jelyman@gmail.com>
2018-09-29 11:52:58 -0700

    list of latest posts after the first on the home page

--- app/index.php
+++ app/index.php
@@ -15,46 +15,133 @@
       echo the_date('F j, Y', '<div class="date-divider"><span>', '</span></div>', 'FALSE');
         get_template_part( 'content', get_post_format() );
         if ($current_post_number == 1 && $paged == 1) { ?>
-          <!-- start link list block -->
-          <div class="link-list-block">
-            <hr class="link-list-divider" />
-            <div class="link-list-header">
-              <h4>More links worth checking out</h4> <i class="far fa-fw fa-external-link"></i>
-            </div>
-            <div class="link-list">
-              <?php
-                // WP_Query arguments
-                $ll_query_args = array(
-                  'post_type'              => array( 'list_link' ),
-                  'paged'                  => '1',
-                  'posts_per_page'         => '10',
-                );
 
-                // The Query
-                $ll_query = new WP_Query( $ll_query_args );
-                
-                while($ll_query->have_posts()) : $ll_query->the_post(); 
-                  $meta_value = get_post_meta( $post->ID, 'external_link', true ); ?>
-                  <p><a href="<?php echo $meta_value; ?>"><?php the_title(); ?></a> <i class="far fa-fw fa-arrow-right icon"></i></p>
-                <?php endwhile; 
-              ?>
+          <!-- start latest in category -->
+          <div class="latest-in-cat">
+            <div class="lic-top">
+            <i class="far fa-fw fa-folder-open"></i> <h3>The latest</h3> <span>from each category.</span>
             </div>
-            <hr class="link-list-divider" />
+            <div class="lic-bottom">
+              <div class="lic-cat-list lic-links">
+                <div class="lic-cat-list-title">
+                <i class="far fa-fw fa-link"></i> <h4>Links</h4>
+                </div>
+                <ul class="fa-ul">
+                <?php 
+                  // get the latest link posts
+                  $lic_links_args = array(
+                    'nopaging'               => false,
+                    'paged'                  => '1',
+                    'posts_per_page'         => '5',
+                    'tax_query'              => array(
+                      'relation' => 'AND',
+                      array(
+                        'taxonomy'         => 'category',
+                        'terms'            => 'links',
+                        'field'            => 'slug',
+                      ),
+                    ),
+                  );
+                  $lic_links_query = new WP_Query( $lic_links_args );
+                  
+                  while($lic_links_query->have_posts()) : $lic_links_query->the_post(); 
+                  $meta_value = get_post_meta( $post->ID, 'external_link', true ); ?>
+                    <li><span class="fa-li"><i class="far fa-chevron-circle-right"></i></span><a href="<?php echo $meta_value; ?>"><?php the_title(); ?></a></li>
+                <?php endwhile; ?>
+                </ul>
+              </div>
+              <div class="lic-cat-list lic-posts">
+                <div class="lic-cat-list-title">
+                <i class="far fa-fw fa-pencil"></i> <h4>Posts</h4>
+                </div>
+                <ul class="fa-ul">
+                  <?php 
+                    // get the latest link posts
+                    wp_reset_query();
+                    $lic_posts_args = array(
+                      'nopaging'               => false,
+                      'paged'                  => '1',
+                      'posts_per_page'         => '5',
+                      'tax_query'              => array(
+                        'relation' => 'AND',
+                        array(
+                          'taxonomy'         => 'category',
+                          'terms'            => 'posts',
+                          'field'            => 'slug',
+                        ),
+                      ),
+                    );
+                    $lic_posts_query = new WP_Query( $lic_posts_args );
+                    
+                    while($lic_posts_query->have_posts()) : $lic_posts_query->the_post(); ?>
+                      <li><span class="fa-li"><i class="far fa-chevron-circle-right"></i></span><a href="<?php echo the_permalink() ?>"><?php the_title(); ?></a></li>
+                  <?php endwhile; ?>
+                </ul>
+              </div>
+              <div class="lic-cat-list lic-reviews">
+                  <div class="lic-cat-list-title">
+                  <i class="far fa-fw fa-glasses"></i> <h4>Reviews</h4>
+                  </div>
+                <ul class="fa-ul">
+                <?php 
+                  // get the latest link posts
+                  wp_reset_query();
+                  $lic_reviews_args = array(
+                    'nopaging'               => false,
+                    'paged'                  => '1',
+                    'posts_per_page'         => '5',
+                    'tax_query'              => array(
+                      'relation' => 'AND',
+                      array(
+                        'taxonomy'         => 'category',
+                        'terms'            => 'reviews',
+                        'field'            => 'slug',
+                      ),
+                    ),
+                  );
+                  $lic_reviews_query = new WP_Query( $lic_reviews_args );
+                  
+                  while($lic_reviews_query->have_posts()) : $lic_reviews_query->the_post(); ?>
+                    <li><span class="fa-li"><i class="far fa-chevron-circle-right"></i></span><a href="<?php echo the_permalink() ?>"><?php the_title(); ?></a></li>
+                <?php endwhile; ?>
+                </ul>
+              </div>
+              <div class="lic-cat-list lic-videos">
+                  <div class="lic-cat-list-title">
+                  <i class="far fa-fw fa-film"></i> <h4>Videos</h4>
+                  </div>
+                <ul class="fa-ul">
+                <?php 
+                  // get the latest link posts
+                  wp_reset_query();
+                  $lic_reviews_args = array(
+                    'nopaging'               => false,
+                    'paged'                  => '1',
+                    'posts_per_page'         => '5',
+                    'tax_query'              => array(
+                      'relation' => 'AND',
+                      array(
+                        'taxonomy'         => 'category',
+                        'terms'            => 'videos',
+                        'field'            => 'slug',
+                      ),
+                    ),
+                  );
+                  $lic_reviews_query = new WP_Query( $lic_reviews_args );
+                  
+                  while($lic_reviews_query->have_posts()) : $lic_reviews_query->the_post(); ?>
+                    <li><span class="fa-li"><i class="far fa-chevron-circle-right"></i></span><a href="<?php echo the_permalink() ?>"><?php the_title(); ?></a></li>
+                <?php endwhile; ?>
+                </ul>
+              </div>
+            </div>
           </div>
-          <!-- end link list block -->
+          <!-- end latest in category -->
         <?php } $current_post_number++;
       endwhile; 
     endif; ?>
     <div class="pagination">
-      <div class="later">
-        <?php previous_posts_link( '<i class="far fa-angle-double-left"></i> Previous Page' ); ?>
-      </div>
-      <div class="count">
-        &nbsp;
-      </div>
-      <div class="earlier">
-        <?php next_posts_link( 'Next Page <i class="far fa-angle-double-right"></i>' ); ?>
-      </div>
+      <?php wp_pagenavi(); ?>
     </div>
 </div>
 <!-- / body -->
